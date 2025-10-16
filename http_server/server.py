85e74c90d2b831bb5dc53d7f43e3c641a074564b
Sergiu <53493788+sergedbs@users.noreply.github.com>
2025-10-16 17:37:10 +0300

    Implement socket creation

--- http_server/server.py
+++ http_server/server.py
@@ -1,5 +1,27 @@
+import socket
+
+HOST = "0.0.0.0"
+PORT = 8080
+
+
+def create_socket(host: str, port: int) -> socket.socket:
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    server.bind((host, port))
+    server.listen(5)
+    server.setblocking(True)
+    return server
+
+
+def start_server():
+    server = create_socket(HOST, PORT)
+    print(f"Starting server on {HOST}:{PORT}")
+    input("")
+    server.close()
+
+
 def main():
-    print("Hello, World!")
+    start_server()
 
 
 if __name__ == "__main__":
